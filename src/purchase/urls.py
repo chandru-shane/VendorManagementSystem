63839de27ac8a51b9d1167f857203304667d21fc
chan <chandru@convin.ai>
2023-12-17 12:02:47 +0530

    acknowledge api updated

--- src/purchase/urls.py
+++ src/purchase/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PurchaseOrderListCreateAPIView, PurchaseOrderRetrieveUpdateDestoryAPIView
+from .views import PurchaseOrderListCreateAPIView, PurchaseOrderRetrieveUpdateDestoryAPIView, AcknowledgeAPIView
 
 
 urlpatterns = [
     path("", PurchaseOrderListCreateAPIView.as_view(), name="po-list-create"),
     path("<int:po_number>/", PurchaseOrderRetrieveUpdateDestoryAPIView.as_view(), name="po-rud"),
-    # path("{po_id}/acknowledge/")
+    path("<int:po_number>/acknowledge/", AcknowledgeAPIView.as_view(), name="acknowledge")
 ]
--- src/purchase/views.py
+++ src/purchase/views.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 from .models import PurchaseOrder
 from .serializers import PurchaseOrderSerializer
 # Create your views here.
@@ -21,3 +24,15 @@ class PurchaseOrderRetrieveUpdateDestoryAPIView(generics.RetrieveUpdateDestroyAP
     serializer_class = PurchaseOrderSerializer
     permission_classes = [IsAuthenticated,] 
     lookup_field = "po_number"
+
+
+class AcknowledgeAPIView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated,]
+    serializer_class = PurchaseOrderSerializer
+    queryset = PurchaseOrder.objects.all()
+    
+    def post(self, request, po_number, *args, **kwargs):
+        po = generics.get_object_or_404(PurchaseOrder, po_number=po_number)
+        po.acknowledgement_date = datetime.now()
+        po.save()
+        return Response({"success":True}, status=status.HTTP_200_OK)
